b552b2e6d6451e030c9ae559f9ba6a52f59f463d
acion <andrei.ion@oracle.com>
2017-03-10 16:44:06 +0200

    initial

---
+++ js/viewModels/components/newhires.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'promise', 'ojs/ojlistview', 
+            'ojs/ojgauge'], 
+        function(oj, ko, $) {
+
+    function NewHiresViewModel() {
+    }
+
+    return NewHiresViewModel;
+
+});
---
+++ js/viewModels/components/totalnumberemps.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 
+                'ojs/ojinputnumber'], 
+        function(oj, ko, $) {
+
+    function TotalNumber() {
+        this.total_number_emps = 17;
+    }
+
+    return TotalNumber;
+
+});
